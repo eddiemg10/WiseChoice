5487a186df76f7ed646238a97d1d4f8aa3026431
eddiemg10 <59659920+eddiemg10@users.noreply.github.com>
2022-10-26 09:23:12 +0300

    Added Material2

--- app/src/main/java/com/example/wisechoice/Explore.kt
+++ app/src/main/java/com/example/wisechoice/Explore.kt
@@ -4,20 +4,36 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.wisechoice.ui.theme.WiseChoiceTheme
 
 
@@ -66,13 +82,84 @@ fun ExplorePage(){
                 .fillMaxWidth()
                 .height(20.dp))
 
+            val text = ""
+
             Surface(
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-                    .height(56.dp),
-
             ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
+
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(0.8f)
+                        .border(BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
+                        ),
+                        value = "",
+                        onValueChange = {
+                            /*TODO*/
+                        },
+                        placeholder = {
+                            Text(
+                                modifier = Modifier.alpha(ContentAlpha.medium),
+                                text = "Search Courses",
+                                color = MaterialTheme.colorScheme.onBackground
+                            )
+                        },
+                        singleLine = true,
+                        leadingIcon = {
+                            IconButton(
+                                modifier = Modifier.alpha(ContentAlpha.medium),
+                                onClick = {}
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "Search Icon",
+                                    tint = MaterialTheme.colorScheme.onBackground
+                                )
+                            }
+                        },
+                        trailingIcon = {
+                            IconButton(
+                                onClick = {
+                                    if (text.isNotEmpty()) {
+                                        // Call onTextChange()
+                                    } else {
+                                        // Call onCloseClickedd()
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "Close Icon",
+                                    tint = MaterialTheme.colorScheme.onBackground
+
+                                )
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            //Change device keypad to have a search icon
+                            imeAction = ImeAction.Search
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onSearch = {
+                                // Call onSearchClicked(text)
+                            }
+                        ),
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = MaterialTheme.colorScheme.background,
+                            cursorColor = MaterialTheme.colorScheme.primary,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                        ),
+                        shape = RoundedCornerShape(18.dp),
+
+                        )
+                }
             }
         }
     }
